ee0915f79bde5bfd73c3b66561bf4ed734ca0243
Tolanee <68304958+Tolanee@users.noreply.github.com>
2021-07-26 22:13:24 +0100

    animations and scroll to top

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const burger = document.querySelector("#clicked");
 const navLinks = document.querySelector(".main-nav-link");
-const burgerHome = document.querySelector(".burger-home");
 
 
 burger.addEventListener("click", ()=> {
   navLinks.classList.toggle("open");
 });
+
+var thisButton = document.getElementById("myBtn");
+thisButton.addEventListener("click", goToTop)
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    thisButton.style.display = "block";
+  } else {
+    thisButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function goToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
