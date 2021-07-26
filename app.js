def22b8cada8cac0abf83b3404786516c58adbce
Tolanee <68304958+Tolanee@users.noreply.github.com>
2021-07-26 07:29:10 +0100

    minimal change

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const burger = document.querySelector("#clicked");
 const navLinks = document.querySelector(".main-nav-link");
 const burgerHome = document.querySelector(".burger-home");
 
-// burgerHome.addEventListener("click", ()=> {
-//   navLinks.classList.toggle("open");
-// });
+
 burger.addEventListener("click", ()=> {
   navLinks.classList.toggle("open");
 });
